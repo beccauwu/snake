332484197c7007d029b514d8d1036653ab0b216a
beccauwu <rebecca@perttula.co>
2022-06-19 13:04:37 +0200

    install firebase, add reference to js file

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,3 +1,32 @@
+// database
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+  // The value of `databaseURL` depends on the location of the database
+  databaseURL: "https://snake-594e4-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+function writeGameData(name, score) {
+    const db = getDatabase();
+    set(ref(db), {
+        player: name,
+        points: score,
+    }
+
+    )
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
+
 // colours
 
 let dark = '#2f3037ff';
